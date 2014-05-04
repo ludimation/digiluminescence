07578858243394c21d54b1c362a7c366b2dc5799
ludimation <mail@ludimotion.com>
2014-05-04 11:36:03 -0400

    Updated comments and added more detailed TODO's for optimization.

--- MATLAB/drawPoints.m
+++ MATLAB/drawPoints.m
@@ -38,10 +38,18 @@ function [ I ] = drawPoints(p_array, I, sz_draw, c_rgb)
     b_array = ones(sz_x_array, 'double')     *3         ; 
     frame_array = double(reshape(repmat(1:p_frames, [p_w,1,1]), sz_x_array));
 
+    % TODO: add additional points to array to account for "sz_draw"
+    
     % clamp x and y arrays at 0 to height & width of image
     x_array = max(min(x_array,size(I, 1)),1);
     y_array = max(min(y_array,size(I, 2)),1);
      
+    % TODO: 
+    %   - concatenate all arays so sub2in() only has to be called once
+    %   - create a c_rgb_array that repmats c_rgb correctly so pixel
+    %   coloring call two steps below only has to be called once
+    
+    
     % create one array for each channel that contains the indexes of the
     % pixels to be colored
     point_indexes_r = sub2ind(sz_I, x_array, y_array, r_array, frame_array);
@@ -53,7 +61,8 @@ function [ I ] = drawPoints(p_array, I, sz_draw, c_rgb)
     I_px(point_indexes_g) = c_rgb(2);
     I_px(point_indexes_b) = c_rgb(3);
 
-    % TODO: add points to points array to account for point size 
+    % TODO: replace this (likely the slowest part) by adding points to
+    % points array to account for point size before finding indexes
     % start drawing at the top left of the point
     sz_draw_half = round(sz_draw/2);
     I_px = circshift(I_px, [-sz_draw_half,-sz_draw_half]);
