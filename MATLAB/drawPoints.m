067c376bbc89afe03a6ec6fecbcd18f35e69387a
ludimation <mail@ludimotion.com>
2014-05-05 00:43:43 -0400

    Updated comments and added stub code for drawPoints optimizations.

--- MATLAB/drawPoints.m
+++ MATLAB/drawPoints.m
@@ -40,15 +40,23 @@ function [ I ] = drawPoints(p_array, I, sz_draw, c_rgb)
 
     % TODO: add additional points to array to account for "sz_draw"
     
-    % clamp x and y arrays at 0 to height & width of image
+    % clamp x and y arrays from 1 to height & width of image
     x_array = max(min(x_array,size(I, 1)),1);
     y_array = max(min(y_array,size(I, 2)),1);
      
-    % TODO: 
-    %   - concatenate all arays so sub2in() only has to be called once
-    %   - create a c_rgb_array that repmats c_rgb correctly so pixel
-    %   coloring call two steps below only has to be called once
-    
+%     % TODO: 
+%     %   - concatenate all arays so sub2in() only has to be called once
+%     %   - create a c_rgb_array that repmats c_rgb correctly so pixel
+%     %   coloring call two steps below only has to be called once
+%     xxx_array = repmat(x_array, [3, 1]);
+%     yyy_array = repmat(y_array, [3, 1]);
+%     rgb_array = [r_array; g_array; b_array];
+%     frame_rgb_array = repmat(frame_array, [3, 1]);
+%     c_rgb_array = reshape(repmat(c_rgb, size(x_array)), rgb_array);
+% 
+%     point_indexes = sub2ind(sz_I, xxx_array, yyy_array, rgb_array, frame_rgb_array);
+%     
+%     I_px(point_indexes) = c_rgb_array(point_indexes);
     
     % create one array for each channel that contains the indexes of the
     % pixels to be colored
